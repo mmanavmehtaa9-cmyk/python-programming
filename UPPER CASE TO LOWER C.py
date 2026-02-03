d12030d496724d4a5819fdfe540178644ba48020
mmanavmehtaa9-cmyk <mmanavmehtaa9@gmail.com>
2026-02-03 12:13:17 +0530

    Add files via upload

---
+++ UPPER CASE TO LOWER C.py
@@ -0,0 +1,10 @@
+print("Uppercase alphabets:")
+for i in range(65,91):
+    print(chr(i), end=" ")
+
+print("\n")
+
+print("Lowercase alphabets:")
+for i in range(97, 123):
+    print(chr(i), end=" ")
+    
